55486ab5e26d9407cf38cfcee771232b49817216
Johan Klinge <johan.klinge@gmail.com>
2023-10-17 09:54:03 +0200

    Fixed dummy test cases

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,27 +1,25 @@
-import mathlib
- 
 def square(num):
-    return mathlib.cal_square(num)
+    return ( num * num )
 
 # Test case 1
 def test_cal_square_1( ):
     result = square(5)
-    assert == 25
+    assert result == 25
  
  
 # Test case 2
 def test_cal_square_2( ):
     result = square(6)
-    assert == 36
+    assert result == 36
  
  
 # Test case 3
 def test_cal_square_3( ):
     result = square(7)
-    assert == 49
+    assert result == 49
  
  
 # Test case 4
 def test_cal_square_4( ):
     result = square(8)
-    assert == 64
+    assert result == 64
