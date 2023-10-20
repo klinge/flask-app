1485e6cdd235a3b61491ff4f7409d746a6adaa40
Johan Klinge <johan.klinge@gmail.com>
2023-10-20 12:23:53 +0200

    Cleaned up pagination from routes

--- app/routes.py
+++ app/routes.py
@@ -97,14 +97,10 @@ def user(username):
 
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
-        if posts.has_prev else None
 
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form, prev_url=prev_url, next_url=next_url)
+
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
