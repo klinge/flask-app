21b2213024913aadef09646e3482c74432dbe4eb
Johan Klinge <johan.klinge@gmail.com>
2023-10-20 11:16:18 +0200

    Added config values for bootstrap

--- config.py
+++ config.py
@@ -18,6 +18,13 @@ class Config(object):
     #: flask login - set days for the 'remember me' cookie
     COOKIE_DURATION = timedelta(days=1)
 
+    #: bootstrap config
+    BOOTSTRAP_BOOTSWATCH_THEME = 'lux'
+    BOOTSTRAP_BTN_STYLE = 'primary'
+    BOOTSTRAP_BTN_SIZE = 'md'
+    BOOTSTRAP_ICON_SIZE = '1em'
+    BOOTSTRAP_ICON_COLOR = 'currentColor'
+
     #: mail settings
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
