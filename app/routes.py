fda8626938084b07860358b9803efa8b639abb36
Johan Klinge <johan.klinge@gmail.com>
2023-10-24 09:59:00 +0200

    Adding package flask-moment

--- app/routes.py
+++ app/routes.py
@@ -47,11 +47,7 @@ def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    next_url = url_for('explore', page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) \
-        if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts, prev_url=prev_url, next_url=next_url)
+    return render_template('index.html', title='Explore', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
