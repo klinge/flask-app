dc19eb3943b6bd40b7f668e8676997c55b6c7165
Johan Klinge <johan.klinge@gmail.com>
2023-10-24 11:25:01 +0200

    Added function to get system locale in site.js

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -1,1 +1,2 @@
+//get locale to use in moment.js, defaults to 'en' if nothing else is found
 const getNavigatorLanguage = () => (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en';
