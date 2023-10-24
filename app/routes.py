2ff2195558ca4c2c551d02099e2db4a2548ba24a
Johan Klinge <johan.klinge@gmail.com>
2023-10-24 15:27:02 +0200

    Finish refactoring auth to blueprint

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,12 @@ from datetime import datetime
 
 from flask import render_template, flash, redirect, url_for
 from flask import request
-#from werkzeug.urls import url_parse
-from urllib.parse import urlparse
 from flask_login import current_user
-from flask_login import login_user, logout_user
 from flask_login import login_required
 
 from app import app
 from app import db
 
-from app.forms import LoginForm, RegistrationForm
 from app.forms import EditProfileForm
 from app.forms import EmptyForm, PostForm
 
@@ -48,42 +44,6 @@ def explore():
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
     return render_template('index.html', title='Explore', posts=posts)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-            user = User.query.filter_by(username=form.username.data).first()
-            if user is None or not user.check_password(form.password.data):
-                flash('Invalid username or password')
-                return redirect(url_for('login'))
-            login_user(user, remember=form.remember_me.data)
-            next_page = request.args.get('next')
-            if not next_page or urlparse(next_page).netloc != '':
-                next_page = url_for('index')
-            return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data.lower())
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
 
 @app.route('/user/<username>')
 @login_required
